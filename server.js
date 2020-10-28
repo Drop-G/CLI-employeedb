ba91ef3e4510f1afc4102aa549f147e9017b5afc
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-27 20:40:28 -0500

    added dependencies and sql connection

---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql")
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Nikeboots4ever",
+    database: "employee_trackerDB"
+  });

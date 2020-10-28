a03519589a8b203edd85748cc3228117aa75162c
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-27 21:12:30 -0500

    updated questions and switch case function

--- server.js
+++ server.js
@@ -9,3 +9,59 @@ const connection = mysql.createConnection({
     password: "Nikeboots4ever",
     database: "employee_trackerDB"
   });
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startPrompt();
+});
+
+function startPrompt() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "Add Role?",
+              "Add Department?",
+              "Update Employee",
+              "Add Employee?",
+              "View All Employees?", 
+              "View All Employee's By Roles?",
+              "View all Emplyees By Deparments"
+            ]
+    }
+]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees?":
+              viewAllEmployees();
+            break;
+    
+          case "View All Employee's By Roles?":
+              viewAllRoles();
+            break;
+          case "View all Emplyees By Deparments":
+              viewAllDepartments();
+            break;
+          
+          case "Add Employee?":
+                addEmployee();
+              break;
+
+          case "Update Employee":
+                updateEmployee();
+              break;
+      
+            case "Add Role?":
+                addRole();
+              break;
+      
+            case "Add Department?":
+                addDepartment();
+              break;
+    
+            }
+    })
+}
+
